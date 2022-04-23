3cd7aeeca8dcc37fda7966822e07949f4dc911c0
Rogen Barrow <rogen.barrow@ibis-management.com>
2022-04-22 22:07:08 -0400

    edit fetch

--- src/App.js
+++ src/App.js
@@ -1,3 +1,10 @@
-import logo from './logo.svg';
+import { useEffect } from 'react';
 import './App.css';
 
+const [data, setData] = useState(null);
+const [loading, setLoading] = useState(true);
+const [error, setError] = useState(null);
+
+useEffect(() => 
+    fetch()
+)
