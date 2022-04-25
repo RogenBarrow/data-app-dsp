2cf09872ebf616bea7125ff85361e6a18719c1f0
Rogen Barrow <rogen.barrow@ibis-management.com>
2022-04-25 14:32:01 -0400

    appjs changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import { useEffect } from 'react';
 import './App.css';
 
+export default function App() {
 const [data, setData] = useState(null);
 const [loading, setLoading] = useState(true);
 const [error, setError] = useState(null);
 
-useEffect(() => 
-    fetch()
-)
+useEffect(() => {
+    fetch().then((response) => console.log(response));
+  }, []);
+
+return <div className="App"></div>
+}
