dfc20da20e9cf750def6c220f944fbfbf7382cce
Devratb <93407364+Devratb@users.noreply.github.com>
2022-04-25 21:56:26 -0400

    fetch but didn't display

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 export default function App() {
@@ -6,14 +6,35 @@ const [data, setData] = useState(null);
 const [loading, setLoading] = useState(true);
 const [error, setError] = useState(null);
 
+
 useEffect(() => {
-  fetch('http://localhost;32/data')
-   .then((response) => response.json())
+  fetch('http://localhost:32/data')
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error(
+        `This is an HTTP error: The status is ${response.status}`
+      );
+    }
+    return response.json();
+  })
    .then((actualData) => console.log(actualData))
    .catch((err) => {
-    console.log(err.message);
-   });
- }, []);
+    setError(err.message);
+    setData(null);
+  })
+  .finally(() => {
+    setLoading(false);
+  });
+}, []);
 
-return <div className="App"></div>
+return <div className="App">
+<h1>API Test</h1>
+{loading && <div>A moment please...</div>}
+      {error && (
+        <div>{`There is a problem fetching the post data - ${error}`}</div>
+      )}
+      <ul>
+      
+      </ul>
+</div>
 }
