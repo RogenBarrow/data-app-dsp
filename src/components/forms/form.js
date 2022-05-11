72c196217469f8b7f4be08c87cac048ef1378c28
Rogen Barrow <rogenbarrow@hotmail.com>
2022-05-11 16:09:37 -0400

    form update

--- src/components/forms/form.js
+++ src/components/forms/form.js
@@ -3,33 +3,26 @@ import { useState } from "react";
 //import './components/css/form.css';
 
 
-function form() {
-    const [values, setValues] = useState({
-        firstName: '',
-        lastName: '',
-    });
+export function form() {
+const [name, setName] = useState("");
 
-    
+const handleSubmit = (event) => {
+    event.preventDefault();
+    alert(`The name you entered was: ${name}`)
+}
 
-
-       return (
-        <div className="wrapper">
-            <h1>Registration Form</h1>
-            <form>
-                <fieldset>
-                    <label>
-                        <p>Name</p>
-                        <input value={values.firstName} name="name" />
-                    </label>
-                    <label>
-                        <p>Last Name</p>
-                        <input value={values.lastName} lastName="lastName" />
-                    </label>
-                </fieldset>
-                <button type="submit">Submit</button>
-            </form>
-        </div>
-    )
-};
+return (
+<form onSubmit={handleSubmit}>
+    <label> Enter your name:
+        <input 
+        type="text"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+        />
+    </label>
+    <input type="submit" />
+</form>
+)
+}
 
 export default form;
