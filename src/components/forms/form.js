3dcd209a5948de87876abe370c3db3abe8caccd6
Devratb <93407364+Devratb@users.noreply.github.com>
2022-06-16 20:43:53 -0400

    last forms

--- src/components/forms/form.js
+++ src/components/forms/form.js
@@ -1,29 +1,69 @@
 import React from "react";
 import { useState } from "react";
 import '/Users/rogenbarrow/Documents/Projects/data-dsp/data-app-dsp/src/css/form.css';
+import axios from 'axios';
 
+function App() {
 
-const Form = () => {
-const [name, setName] = useState("");
+    const [fullname, setFullname] = useState({
+        firstName: "",
+        lastName: ""
+    })
 
-const handleSubmit = (event) => {
-    event.preventDefault();
-    alert(`The name you entered was: ${name}`)
+    const axiosPost = () => {
+        const requestData = fullname;
+        axios
+            .post("http://localhost:32/user", requestData)
+            .then(({ data }) => console.log(data))
+            .catch((err) => console.error(err));
+    }
+    
+    function handleChange(event) {
+        const newValue = event.target.value;
+        const inputName = event.target.name;
+
+        setFullname(prevValue => {
+            if (inputName === "firstname") {
+                return {
+                    firstName: newValue,
+                    lastName:   prevValue
+                }
+            } else if (inputName === "lastname") {
+                return {
+                    firstName: prevValue,
+                    lastName: newValue
+                }
+            }
+        })
+        
+        console.log(fullname);
+        //console.log(newValue);
+        //console.log(inputName);
+        //console.log(setFullname);
+    };
+
+    const newData = JSON.stringify(fullname);
+
+    const handleClick = newData => {
+
+        fetch("http://localhost:32/user", {
+            method: 'POST',
+            headers: { 'Content-Type': 'text/html' },
+            body: newData
+        });
+        
+    };
+    
+    
+    return(
+        <div>
+            <form onSubmit={() => {}}>
+                <input onChange={handleChange} value={fullname.firstname} type="text" name="firstname" placeholder="First Name"/>
+                <input onChange={handleChange} value={fullname.lastname} type="text" name="lastname" placeholder="Last Name"/>
+                <button  class="button" onClick={axiosPost}>Submit</button>
+            </form>
+        </div>
+    );
 }
 
-return (
-<form className="form" onSubmit={handleSubmit}>
-    <label className="labelName">Enter your name</label>
-    <label>
-        <input 
-        type="text"
-        value={name}
-        onChange={(e) => setName(e.target.value)}
-        />
-    </label>
-    <input className="button" type="submit" />
-</form>
-)
-};
-
-export default Form;
+export default App;
