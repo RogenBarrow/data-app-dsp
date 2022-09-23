679034a0b242fb0f77c3bd21806ba5f490902c9a
Devratb <93407364+Devratb@users.noreply.github.com>
2022-09-23 15:46:30 -0400

    last touch

--- src/components/datas/data.js
+++ src/components/datas/data.js
@@ -1,27 +1,63 @@
 import React from "react";
+import { useState } from "react";
 import axios from "axios";
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
+import DataTable from 'react-data-table-component';
 
 
 function App() {
-
+        //getter      //setter
+const [displayItems, setDisplayItems] = useState("");
     
+const columns = [
+    {
+        name: '_id',
+        selector: row => row._id,
+    },
+    {
+        name: 'Fistname',
+        selector: row => row.name,
+    },
+    {
+        name: 'Lastname',
+        selector: row => row.lastName,
+    },
+];
+
+const data = displayItems;
+
+
     //GET Request
     const axiosGet = () => {
 
         axios
             .get("http://localhost:32/data",)
-            .then(function(data) {
-                console.log(data)
+            .then(function(responseObject) {
+                const users = responseObject.data;
+                setDisplayItems(users);
             })
             .catch((err) => console.error(err));
     }
+
+    //const stringifyData = JSON.parse(displayItems);
+    console.table(displayItems);
+    //console.log(stringifyData);
+    //console.log(displayItems)
+
+    const listName = displayItems;
+    console.log(listName);
 
 
     return(
 
         <Box>
+            <DataTable
+            columns={columns}
+            data={data}
+        />
+            
+            
             <Button onClick={axiosGet}>Get Data</Button>
         </Box>
 
